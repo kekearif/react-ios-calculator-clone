a9785df4debf4274de78a70a69e10ba56ead1fff
keke Arif <kekearif@googlemail.com>
2020-04-09 13:36:28 +0800

    Added fork me image to top panel

--- src/component/TopPanel.js
+++ src/component/TopPanel.js
@@ -2,7 +2,18 @@ import React from 'react';
 import '../App.css';
 
 function TopPanel() {
-  return <div className="Top-panel"></div>;
+  return (
+    <div className="Top-panel">
+      <a href="https://github.com/kekearif/react-ios-calculator-clone">
+        <img width="149"
+             height="149"
+             src="https://github.blog/wp-content/uploads/2008/12/fo
+rkme_left_white_ffffff.png?resize=149%2C149"
+             class="attachment-full size-full" alt="Fork me on GitHub"
+             data-recalc-dims="1" />
+      </a>
+    </div>
+  );
 }
 
 export default TopPanel;
