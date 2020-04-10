8b430e8356d39138232935413953f77874ce40d2
keke Arif <kekearif@googlemail.com>
2020-04-10 23:22:12 +0800

    Minor modifications

--- src/component/TopPanel.js
+++ src/component/TopPanel.js
@@ -9,7 +9,7 @@ function TopPanel() {
              height="149"
              src="https://github.blog/wp-content/uploads/2008/12/fo
 rkme_left_white_ffffff.png?resize=149%2C149"
-             class="attachment-full size-full" alt="Fork me on GitHub"
+             className="attachment-full size-full" alt="Fork me on GitHub"
              data-recalc-dims="1" />
       </a>
     </div>
